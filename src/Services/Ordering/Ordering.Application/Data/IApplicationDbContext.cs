c048f933419ed7cae6dbaec49693d0bdc2e8fec0
Özmen, Funda <UI513825@rwe-ag.com>
2024-08-29 14:28:02 +0300

    Ordering.Application implementation

---
+++ src/Services/Ordering/Ordering.Application/Data/IApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Models;
+using Ordering.Domain.ValueObjects;
+
+namespace Ordering.Application.Data
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Order> Orders { get; }
+        DbSet<Customer> Customers { get; }
+        DbSet<Product> Products { get; }
+        DbSet<OrderItem> OrderItems { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
--- src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
+++ src/Services/Ordering/Ordering.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Ordering.Application.Data;
 using System.Reflection;
 
 namespace Ordering.Infrastructure.Data
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext: DbContext, IApplicationDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) :base(options)
         {
