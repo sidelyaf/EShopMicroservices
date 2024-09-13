a6d6445f58f8fdf26102f370adfac889a573615f
Özmen, Funda <UI513825@rwe-ag.com>
2024-09-13 16:20:29 +0300

    ordering service api integrations

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -8,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 //Add services to the container
 builder.Services.AddApplicationServices().
     AddInfrastructureServices(builder.Configuration)
-    .AddApiServices();
+    .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
